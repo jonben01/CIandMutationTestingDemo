b9f0724fff02286ecaf3bfc4548eafd72444eb3a
Jonas <jonasbendorff@gmail.com>
2026-02-21 15:01:14 +0100

    first batch of tests for cached calc

--- Tests/CachedCalculatorTest.cs
+++ Tests/CachedCalculatorTest.cs
@@ -18,4 +18,79 @@ public class CachedCalculatorTest
         // Assert
         Assert.That(result, Is.EqualTo(5));
     }
+    
+    [Test]
+    public void Subtract()
+    {
+        var calc = new CachedCalculator();
+        var a = 10;
+        var b = 5;
+        var result = calc.Subtract(a, b);
+        Assert.That(result, Is.EqualTo(5));
+    }
+    
+    [Test]
+    public void Multiply()
+    {
+        var calc = new CachedCalculator();
+        var a = 10;
+        var b = 5;
+        var result = calc.Multiply(a, b);
+        Assert.That(result, Is.EqualTo(50));
+    }
+    
+    [Test]
+    public void Divide()
+    {
+        var calc = new CachedCalculator();
+        var a = 10;
+        var b = 5;
+        var result = calc.Divide(a, b);
+        Assert.That(result, Is.EqualTo(2));
+    }
+    
+    [Test]
+    public void DivideByZero_ThrowsException()
+    {
+        var calc = new CachedCalculator();
+        Assert.Throws<DivideByZeroException>(() => calc.Divide(10, 0));
+    }
+    
+    //Input vs expected output
+    [TestCase(0, 1)]
+    [TestCase(1, 1)]
+    [TestCase(5, 120)]
+    public void Factorial_ValidInput_ReturnsCorrectResult(int input, int expected)
+    {
+        var calc = new CachedCalculator();
+        var result = calc.Factorial(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Factorial_NegativeInput_ThrowsException()
+    {
+        var calc = new CachedCalculator();
+        Assert.Throws<ArgumentException>(() => calc.Factorial(-1));
+    }
+    
+    [TestCase(2, true)]
+    [TestCase(3, true)]
+    [TestCase(4, false)]
+    [TestCase(5, true)]
+    [TestCase(6, false)]
+    [TestCase(7, true)]
+    [TestCase(8, false)]
+    [TestCase(9, false)]
+    [TestCase(1, false)]
+    [TestCase(0, false)]
+    [TestCase(-3, false)]
+    public void IsPrime_VariousInputs_ReturnsExpectedResult(int input, bool expected)
+    {
+        var calc = new CachedCalculator();
+
+        var result = calc.IsPrime(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
